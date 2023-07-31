52cc6fed02a1f1d4d2bfaceee1926a852a3b695c
Vanessa Jesik <vajesik@gmail.com>
2023-07-31 16:25:18 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(address) {
+    return Math.abs(42-address)
+}
+
+function distanceFromHqInFeet(address) {
+    return distanceFromHqInBlocks(address) * 264
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs(start-destination)*264
+}
+
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    }
+    else if (distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
